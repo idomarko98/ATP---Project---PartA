d876c9ca62a3ce9f81924a04adcc3b2659d76fd1
Rotem <rotem12430@gmail.com>
2018-04-24 20:53:33 +0300

    24/04/18 20:52 BestFirstSearch + change to AState

--- src/algorithms/search/AState.java
+++ src/algorithms/search/AState.java
@@ -1,10 +1,31 @@
 package algorithms.search;
 
 public abstract class AState implements Comparable {
+
     private int cost;
 
+    public AState(int cost){
+        this.cost = cost;
+    }
+
+    public AState(){
+        this.cost = 0;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(Object o)
+    {
+        if(!(o instanceof AState))
+            return 0;
+        AState aState = (AState)o;
+        return this.cost - aState.cost;
     }
 }
--- src/algorithms/search/BestFirstSearch.java
+++ src/algorithms/search/BestFirstSearch.java
@@ -1,12 +1,21 @@
 package algorithms.search;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class BestFirstSearch extends BreadthFirstSearch {
 
     public BestFirstSearch(){
+        super();
+        openSet = new PriorityQueue<>();
+    }
 
+    @Override
+    protected void addToSet(AState state, AState fatherState) {
+        if(fatherState != null)
+            state.setCost(fatherState.getCost()+1);
+        else
+            state.setCost(0);
+        openSet.add(state);
     }
 
     @Override
