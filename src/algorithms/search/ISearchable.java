97b59c659f7aab1b4cd97308e05a269f2d5ac66f
Ido <idomarko1998@gmail.com>
2018-04-25 17:53:28 +0300

    25/04/18 17:46 part a + part b completed

--- src/algorithms/search/ISearchable.java
+++ src/algorithms/search/ISearchable.java
@@ -2,6 +2,9 @@ package algorithms.search;
 
 import java.util.List;
 
+/**
+ * Interface for a searchable object
+ */
 public interface ISearchable {
    AState getStartState();
    boolean isGoalState(AState state);
